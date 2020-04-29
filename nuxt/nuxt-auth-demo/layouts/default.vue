cc1e075a8fc61d4290405445dcafc282bf043ef2
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2020-04-29 17:20:56 +0900

    Add nuxt-auth-demo

---
+++ nuxt/nuxt-auth-demo/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <nav class="navbar navbar-dark bg-dark">
+      <nuxt-link class="navbar-brand" to="/">
+        Authentication Demo
+      </nuxt-link>
+    </nav>
+    <div class="container">
+      <div class="row p-3">
+        <nuxt />
+      </div>
+    </div>
+  </div>
+</template>
